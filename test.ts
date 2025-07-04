f378796938b9d42e33f56cba6fc7089318b4a7cc
Benjamin Häublein <benjaminhaeublein@gmail.com>
2025-07-04 19:14:21 +0200

    ensure that there is a valley behind a ridge

--- test.ts
+++ test.ts
@@ -1,32 +1,61 @@
 class DirectionalView {
-   ridges: RidgePoint[]
+   ridges: ElevatedPoint[]
    highest_elevation: number;
-   add_ridge_point(location, elevation, distance): void {
-     this.ridges.push(new RidgePoint(location, elevation, distance));
-     if (elevation > this.highest_elevation) {
-       this.highest_elevation = elevation;
-     }
-   }
+   candidate: RidgeCandidatePoint;
+
    constructor(){
      this.ridges = []
      this.highest_elevation = 0
    }
+
+   add_possible_ridge_point(location, elevation, distance, pass: number): void {
+     if (elevation > this.highest_elevation) {
+       this.highest_elevation = elevation;
+       // if there is a valley behind the last candidate, the last candidate was indeed a RidgePoint
+       if (this.candidate && (this.candidate.pass - pass) > 1) {
+         this.add_ridge_point(this.candidate.point);
+       }
+       this.candidate = new RidgeCandidatePoint(new ElevatedPoint(location, elevation, distance), pass)
+     }
+   }
+   add_ridge_point(ridge_point: ElevatedPoint): void {
+     this.ridges.push(ridge_point);
+     if (ridge_point.elevation > this.highest_elevation) {
+       this.highest_elevation = ridge_point.elevation;
+     }
+   }
+   
+   capture_last_ridge(pass: number): void {
+     if (this.candidate && (this.candidate.pass - pass) > 1) {
+       this.add_ridge_point(this.candidate.point);
+     }
+   }
 }
 
-interface RidgePoint {
+class ElevatedPoint {
   location: Location
   elevation: number
-  distance_to_location: number;
+  distance_to_central_location: number;
   
   constructor(location: Location, elevation: number, distance: number) {
     this.location = location;
     this.elevation = elevation;
-    this.distance = distance;
+    this.distance_to_central_location = distance;
   }
 }
 
+class RidgeCandidatePoint {
+  point: ElevatedPoint;
+  pass: number;
+  constructor (point, pass) {
+    this.point = point;
+    this.pass = pass;
+  }
+}
+ 
+
 // indexed by directions
-type Ridge = Array<RidgePoint>
+type Ridge = Array<ElevatedPoint>
 
 //todo implement it!
 interface Location {
@@ -61,9 +90,11 @@ class View {
     this.data = data;
     this.location = location;
     this.init_directions()
-    for (let i = 1; i < (this.visual_range/this.data_steps); i++) {
+    const max_pass = (this.visual_range/this.data_steps);
+    for (let i = 1; i < max_pass ; i++) {
       this.traverse_one_ring(i);
     }
+    this.finish_directions(max_pass);
     this.build_ridges();
   }
 
@@ -89,11 +120,22 @@ class View {
     }
   }
 
-  check_point(location: Location) {
+  finish_directions(pass): void {
+    for (let i = 0; i < 4 * this.circle_resolution; i++) {
+      this.directions[i].capture_last_ridge(pass);
+    }
+  }
+
+  check_point(location: Location, pass: number) {
     const direction = this.get_direction(location);
     const elevation = this.get_elevation(location)
     if (elevation > this.directions[direction].highest_elevation) {
-      this.directions[direction].add_ridge_point(location, elevation, this.location.distance_to(location));
+      this.directions[direction].add_possible_ridge_point(
+        location, 
+        elevation, 
+        this.location.distance_to(location),
+        pass
+      );
     }
   }
 
@@ -105,22 +147,22 @@ class View {
     //move right
     for (let i=0; i<(2*i+1); i++) {
       location.move_lat(this.data_steps);
-      this.check_point(location);
+      this.check_point(location, distance);
     }
     //move down
     for (let i=0; i<(2*i); i++) {
+      location.move_lon(this.data_steps, distance);
       this.check_point(location);
-      location.move_lon(this.data_steps);
     }
     //move left
     for (let i=0; i<(2*i); i++) {
+      location.move_lat(-this.data_steps, distance);
       this.check_point(location);
-      location.move_lat(-this.data_steps);
     }
     //move up
     for (let i=0; i<(2*i); i++) {
+      location.move_lon(-this.data_steps, distance);
       this.check_point(location);
-      location.move_lon(-this.data_steps);
     }
   }
   

9e60c4f9ad6e1c28cce29e4f4f4d6b99270a075e
Benjamin Häublein <benjaminhaeublein@gmail.com>
2025-07-05 10:38:57 +0200

    separate src files

--- src/dataSource.ts
+++ src/dataSource.ts
@@ -15,8 +15,27 @@ export default class DataSource {
     this.maxPoint = new GeoLocation(central_location.lat, central_location.lon);
     this.maxPoint.move_lat(distance);
     this.maxPoint.move_lon(distance);
-    this.tileset = new SyncTileSet('./cache/', [this.minPoint.lat, this.minPoint.lon], [this.maxPoint.lat, this.maxPoint.lon], function (err) { if (err) {console.log(err); }});
-    console.log(this.get_elevation(central_location.lat, central_location.lon));
+    console.log(this.minPoint);
+    console.log(this.maxPoint);
+  }
+  
+  async init_tileset(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.tileset = new SyncTileSet(
+        './cache/', 
+        [this.minPoint.lat, this.minPoint.lon], 
+        [this.maxPoint.lat, this.maxPoint.lon], 
+        (err) => { 
+          if (err) {
+            console.log(err); 
+            reject(err); 
+            return 
+          } 
+          console.log(this.tileset.getElevation([central_location.lat, central_location.lon]));
+          console.log(this.get_elevation(central_location.lat, central_location.lon));
+          resolve(); 
+        });
+    });
   }
   
   get_elevation(lat, lon: number): number {

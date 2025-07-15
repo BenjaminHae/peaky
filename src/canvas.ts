006de9b0d56264efc5c793032d9cb262fe6a8977
Benjamin Häublein <benjaminhaeublein@gmail.com>
2025-07-15 19:58:27 +0200

    rename context to ctx to match documentation, rotate names, add complex path interface

--- src/canvas.ts
+++ src/canvas.ts
@@ -9,7 +9,7 @@ export default class Canvas {
   width: number;
   height: number;
   canvas: any;
-  context: any;
+  ctx: any;
   max_distance: number;
   scaling: number
   
@@ -18,10 +18,10 @@ export default class Canvas {
     this.height = height;
     this.scaling = scaling;
     this.canvas = createCanvas(width*scaling, height);
-    this.context = this.canvas.getContext('2d');
-    this.context.fillStyle = "white";
-    this.context.fillRect(0, 0, width*scaling, height);
-    this.context.fillStyle = "black";
+    this.ctx = this.canvas.getContext('2d');
+    this.ctx.fillStyle = "white";
+    this.ctx.fillRect(0, 0, width*scaling, height);
+    this.ctx.fillStyle = "black";
   }
 
   invert_point(y: number): number {
@@ -29,45 +29,69 @@ export default class Canvas {
   }
 
   paintDot(x,y: number, size:number, distance?: number) {
-    //this.context.fillStyle = `rgb(
+    //this.ctx.fillStyle = `rgb(
     //    ${Math.floor(255 - 42.5 * distance/max_distance)}
     //    ${Math.floor(255 - 42.5 * j)}
     //    0)`;
-    this.context.fillStyle = "black";
-    this.context.fillRect(x*this.scaling,this.invert_point(y),size,size);
+    this.ctx.fillStyle = "black";
+    this.ctx.fillRect(x*this.scaling,this.invert_point(y),size,size);
+  }
+
+  startPaintPath(direction, projected_height: number) {
+    this.ctx.fillStyle = "black";
+    this.ctx.lineWidth = 3;
+    this.ctx.beginPath();
+    this.ctx.moveTo(direction * this.scaling, this.invert_point(projected_height));
+  }
+
+  nextPaintPath(direction, projected_height) {
+    this.ctx.lineTo(direction * this.scaling, this.invert_point(projected_height));
+  }
+
+  endPaintPath() {
+    this.ctx.stroke();
   }
   
   paintLine(points: Array<Point>) {
-    this.context.fillStyle = "black";
-    this.context.beginPath();
-    this.context.moveTo(points[0].x * this.scaling, this.invert_point(points[0].y));
+    this.ctx.fillStyle = "black";
+    this.ctx.beginPath();
+    this.ctx.moveTo(points[0].x * this.scaling, this.invert_point(points[0].y));
     for (let i = 1; i < points.length; i++) {
-      this.context.lineTo(points[i].x * this.scaling, this.invert_point(points[i].y));
+      this.ctx.lineTo(points[i].x * this.scaling, this.invert_point(points[i].y));
     }
-    this.context.stroke();
+    this.ctx.stroke();
   }
 
   paintDirection(name: string, position: number, size = 20) {
-    this.context.fillStyle = "red";
-    this.context.beginPath();
-    this.context.moveTo(position * this.scaling, 0);
-    this.context.lineTo(position * this.scaling + size, 2 * size);
-    this.context.lineTo(position * this.scaling - size, 2 * size);
-    this.context.fill();
-    this.context.font = `${3*size}px serif`;
-    this.context.textAlign = 'center';
-    this.context.fillText(name, position * this.scaling, 5 * size + 1);
+    this.ctx.fillStyle = "red";
+    this.ctx.lineWidth = 3;
+    this.ctx.beginPath();
+    this.ctx.moveTo(position * this.scaling, 0);
+    this.ctx.lineTo(position * this.scaling + size, 2 * size);
+    this.ctx.lineTo(position * this.scaling - size, 2 * size);
+    this.ctx.fill();
+    this.ctx.font = `${3*size}px serif`;
+    this.ctx.textAlign = 'center';
+    this.ctx.fillText(name, position * this.scaling, 5 * size + 1);
     if (position == 0) {
       this.paintDirection(name, this.width, size);
     }
   }
 
-  paintPeak(name: string, elevation: number, direction): void {
-    this.context.font = `$20px serif`;
-    this.context.textAlign = 'center';
-    //this.context.fillText(`${name}\n${elevation.toFixed(0)} m`, direction * this.scaling, this.invert_point(elevation) - 7 * 10 + 1);
-    this.context.fillText(`${name}`, direction * this.scaling, this.invert_point(elevation) - 7 * 10 + 1);
-    this.context.fillText(`${elevation.toFixed(0)} m`, direction * this.scaling, this.invert_point(elevation) - 1.5 * 10 + 1);
+  paintPeak(name: string, elevation, projected_elevation, direction: number): void {
+    this.ctx.fillStyle = "black";
+    this.ctx.lineWidth = 3;
+    this.ctx.beginPath();
+    this.ctx.moveTo(direction * this.scaling, this.invert_point(projected_elevation));
+    this.ctx.lineTo(direction * this.scaling, this.invert_point(projected_elevation) - 80);
+    this.ctx.stroke();
+    this.ctx.save();
+    this.ctx.font = `$20px serif`;
+    this.ctx.textAlign = 'left';
+    this.ctx.translate(direction * this.scaling, this.invert_point(projected_elevation) - 80 - 10);
+    this.ctx.rotate(-60 * Math.PI / 180);
+    this.ctx.fillText(`${name} - ${elevation.toFixed(0)} m`, 0, 0);
+    this.ctx.restore();
   }
   
   store(path: string) {

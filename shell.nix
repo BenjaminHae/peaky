dcd21b342e7642563cb75e543adf431f0ca39159
Benjamin Häublein <benjaminhaeublein@gmail.com>
2025-07-27 09:33:46 +0200

    fix problems with libuuid

--- shell.nix
+++ shell.nix
@@ -2,11 +2,14 @@ with (import <nixpkgs> {});
 mkShell {
   buildInputs = [
     nodejs
+    #libuuid
   ];
+  #env = { LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.libuuid];  };
   shellHook = ''
       mkdir -p .nix-node
       export NODE_PATH=$PWD/.nix-node
       export NPM_CONFIG_PREFIX=$PWD/.nix-node
       export PATH=$NODE_PATH/bin:$PATH
+      export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [ pkgs.libuuid ]}:$LD_LIBRARY_PATH";
   '';
 }
